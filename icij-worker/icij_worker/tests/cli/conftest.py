d449c4bfad33831ee4a1814d4e49e29be6f8c1a4
Clément Doumouro <clement.doumouro@gmail.com>
2024-03-13 16:11:55 +0100

    chore: tesst and publish CI for `icij-common` and `icij-worker`:wq

--- icij-worker/icij_worker/tests/cli/conftest.py
+++ icij-worker/icij_worker/tests/cli/conftest.py
@@ -1,7 +1,13 @@
 import pytest
+import typer
 from typer.testing import CliRunner
 
 
 @pytest.fixture(scope="session")
 def cli_runner() -> CliRunner:
+    # disable rich to avoid colors to show up in test string:
+    # https://github.com/tiangolo/typer/pull/647#issuecomment-1868190451
+    # rich and pytest capture don't play well together:
+    # https://github.com/Textualize/rich/issues/317
+    typer.core.rich = None
     return CliRunner(mix_stderr=False)
