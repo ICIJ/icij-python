b8b0c0f88f99cc66b834ed05c034dc4cb5a10c71
Clément Doumouro <clement.doumouro@gmail.com>
2024-08-07 17:38:59 +0200

    debug

--- icij-worker/icij_worker/__init__.py
+++ icij-worker/icij_worker/__init__.py
@@ -19,12 +19,13 @@ try:
 except ImportError:
     pass
 
-try:
-    from icij_worker.worker.neo4j_ import Neo4jWorker, Neo4jWorkerConfig
-    from icij_worker.event_publisher.neo4j_ import Neo4jEventPublisher
-    from icij_worker.task_manager.neo4j_ import Neo4JTaskManager
-except ImportError as e:
-    pass
+# try:
+from icij_worker.worker.neo4j_ import Neo4jWorker, Neo4jWorkerConfig
+from icij_worker.event_publisher.neo4j_ import Neo4jEventPublisher
+from icij_worker.task_manager.neo4j_ import Neo4JTaskManager
+
+# except ImportError:
+#     pass
 
 try:
     from icij_worker.task_storage.fs import FSKeyValueStorage
