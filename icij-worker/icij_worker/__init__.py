95fa0ed19e89b55a60834f60767a2944e12c0f4a
Clément Doumouro <clement.doumouro@gmail.com>
2024-08-08 12:02:27 +0200

    chore: improve top level imports

--- icij-worker/icij_worker/__init__.py
+++ icij-worker/icij_worker/__init__.py
@@ -15,19 +15,19 @@ from .namespacing import Namespacing
 try:
     from icij_worker.worker.amqp import AMQPWorker, AMQPWorkerConfig
     from icij_worker.event_publisher.amqp import AMQPPublisher
-    from icij_worker.task_manager.amqp import AMQPTaskManager
+    from icij_worker.task_manager.amqp import AMQPTaskManager, AMQPTaskManagerConfig
 except ImportError:
     pass
 
 try:
     from icij_worker.worker.neo4j_ import Neo4jWorker, Neo4jWorkerConfig
     from icij_worker.event_publisher.neo4j_ import Neo4jEventPublisher
-    from icij_worker.task_manager.neo4j_ import Neo4JTaskManager
+    from icij_worker.task_manager.neo4j_ import Neo4JTaskManager, Neo4JTaskManagerConfig
 except ImportError:
     pass
 
 try:
-    from icij_worker.task_storage.fs import FSKeyValueStorage
+    from icij_worker.task_storage.fs import FSKeyValueStorage, FSKeyValueStorageConfig
 except ImportError:
     pass
 
