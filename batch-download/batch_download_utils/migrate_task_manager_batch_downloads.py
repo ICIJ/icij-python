f712be125effa2d96fcabab262d0f912ac5cf013
Bruno Thomas <bthomas@icij.org>
2024-08-21 11:35:22 +0000

    feat: add list handling in rename_field ICIJ/datashare#1399
    Co-authored-by: Maxime Vanza Lutonda <mvanzalu@users.noreply.github.com>

--- batch-download/batch_download_utils/migrate_task_manager_batch_downloads.py
+++ batch-download/batch_download_utils/migrate_task_manager_batch_downloads.py
@@ -4,24 +4,29 @@ from datetime import datetime
 from functools import reduce
 from json import loads, dumps
 from os.path import basename
-from typing import Callable
+from typing import Callable, Any
 from urllib.parse import urlparse, unquote
 
-import redis
+import redis.asyncio as redis
 
 from batch_download_utils.hmap_cleaner import DS_TASK_MANAGER
 
 
 def rename_field(task: dict, old_name: str, new_name: str) -> dict:
-    new_task = dict()
-    for k, v in task.items():
-        if k == old_name:
-            new_task[new_name] = v
-        elif isinstance(v, dict):
-            new_task[k] = rename_field(v, old_name, new_name)
-        else:
-            new_task[k] = v
-    return new_task
+    if isinstance(task, dict):
+        new_task = dict()
+        for k, v in task.items():
+            if k == old_name:
+                new_task[new_name] = v
+            elif isinstance(v, list):
+                new_task[k] = list(map(lambda item: rename_field(item, old_name, new_name), v))
+            elif isinstance(v, dict):
+                new_task[k] = rename_field(v, old_name, new_name)
+            else:
+                new_task[k] = v
+        return new_task
+    else:
+        return task
 
 
 def rename_value(task: dict, old_value: str, new_value: str) -> dict:
@@ -40,15 +45,19 @@ def add_field(task: dict, key, value):
     return new_task
 
 
+def merge_dicts(d1, d2):
+    return {**d1, **{k: merge_dicts(d1[k], d2[k]) if isinstance(d1.get(k), dict) and isinstance(v, dict) else v for k, v in d2.items()}}
+
+
 def move_field(task, source_json_path, dest_json_path) -> dict:
     source_fields = source_json_path.split(".")
     dest_fields = dest_json_path.split(".")
     # get the "leaf" value = final value of the source_json_path
-    value = reduce(lambda d, k: d[k], source_fields, task)
+    value = get_value_from_json_path(task, source_json_path)
     # build one way dest_fields dictionary with the original value {c:value} if we have "a.b.c" path
     dest_path_result = reduce(lambda d, k: {k: d}, reversed(dest_fields[0:-1]), {dest_fields[-1]: value})
     # then merge it with the original task dict copy
-    result = {**task.copy(), **dest_path_result}
+    result = merge_dicts(task, dest_path_result)
     # finally remove original path
     del result[source_fields[0]]
     return result
@@ -56,7 +65,7 @@ def move_field(task, source_json_path, dest_json_path) -> dict:
 
 def get_date_from_task(task: dict) -> datetime:
     if task.get("createdAt"):
-        return datetime.fromtimestamp(task.get("createdAt"))
+        return datetime.fromtimestamp(task.get("createdAt")/1000)
     elif task.get("args") and task.get("args").get("batchDownload"):
         batch_download = task.get("args").get("batchDownload")
         filename_with_path = batch_download.get("filename")[1]
@@ -74,9 +83,27 @@ def get_date_from_task(task: dict) -> datetime:
 
 def change_value(task: dict, dict_path: str, change_func: Callable) -> dict:
     fields = dict_path.split(".")
-    old_value = reduce(lambda d, k: d[k], fields, task)
-    dest_path_result = reduce(lambda d, k: {k: d}, reversed(fields[0:-1]), {fields[-1]: change_func(old_value)})
-    return {**task.copy(), **dest_path_result}
+    result = task.copy()
+    value = result
+    for field_index, field in enumerate(fields):
+        if field.endswith('[]'):
+            list_field = field.replace('[]', '')
+            list_value = value[list_field]
+            for list_index, item in enumerate(list_value):
+                list_value[list_index] = change_value(item, ".".join(fields[field_index+1:]), change_func)
+            break
+        elif field_index == len(fields) - 1:
+            value[field] = change_func(value[field])
+        else:
+            value = value.get(field)
+    return result
+
+
+def get_value_from_json_path(task: dict, path: str) -> Any:
+    try:
+        return reduce(lambda d, k: d[k], path.split("."), task)
+    except KeyError:
+        return None
 
 
 async def main(args: dict) -> None:
@@ -85,18 +112,26 @@ async def main(args: dict) -> None:
     tasks = await client.hgetall(DS_TASK_MANAGER)
     for k, v in tasks.items():
         task = loads(v)
-        new_task = rename_field(task, "@class", "@type")
-        new_task = rename_field(new_task, "properties", "args")
+        new_task = rename_field(task, "properties", "args")
+        if get_value_from_json_path(new_task, "args.batchDownload.projects") is not None:
+            new_task = change_value(new_task, "args.batchDownload.projects", lambda v: v[1])
+            new_task = change_value(new_task, "args.batchDownload.projects[].sourcePath", lambda v: v[1])
+        if get_value_from_json_path(new_task, "args.batchDownload.filename") is not None:
+            new_task = change_value(new_task, "args.batchDownload.filename", lambda v: v[1])
+
+        new_task = rename_field(new_task, "@class", "@type")
         new_task = rename_value(new_task, "org.icij.datashare.asynctasks.TaskView", "Task")
         new_task = add_field(new_task, "retriesLeft", 3)
-        new_task = add_field(new_task, "createdAt", get_date_from_task(task).timestamp())
+        new_task = add_field(new_task, "createdAt", int(get_date_from_task(task).timestamp()*1000))
         new_task = move_field(new_task, "user", "args.user")
-        new_task = change_value(new_task, "args.batchDownload.filename", lambda v: v[1])
-        await client.hset(DS_TASK_MANAGER, task.get("id"), dumps(new_task))
+
+
+        print(dumps(new_task, indent=4))
+        # await client.hset(DS_TASK_MANAGER, task.get("id"), dumps(new_task))
 
 
 def parse_args(argv) -> dict:
-    if len(argv) != 3:
+    if len(argv) != 2:
         print(f"usage: {argv[0]} <redis_url (ex: redis://localhost:6379)>")
         exit(1)
     return {"redis_url": argv[1]}
--- batch-download/batch_download_utils/tests/test_migrate_task_manager_batch_downloads.py
+++ batch-download/batch_download_utils/tests/test_migrate_task_manager_batch_downloads.py
@@ -12,6 +12,14 @@ def test_rename_nested_field():
     assert rename_field({"foo": {"bar": "baz"}}, "bar", "qux") == {"foo": {"qux": "baz"}}
 
 
+def test_rename_nested_list_field():
+    assert rename_field({"foo": [{"key": "val1"}, {"key": "val2"}]}, "key", "new_key") == {"foo": [{"new_key": "val1"}, {"new_key": "val2"}]}
+
+
+def test_rename_nested_list_field_with_non_homogeneous_list():
+    assert rename_field({"foo": ["erk!", {"key": "val1"}]}, "key", "new_key") == {"foo": ["erk!",{"new_key": "val1"}]}
+
+
 def test_rename_value():
     assert rename_value({"foo": "bar"}, "bar", "qux") == {"foo": "qux"}
 
@@ -23,6 +31,10 @@ def test_add_field():
 def test_move_field():
     assert move_field({"foo": "bar"}, "foo", "level1.foo") == {"level1": {"foo": "bar"}}
     assert move_field({"foo": "bar"}, "foo", "level1.level2.foo") == {"level1": {"level2": {"foo": "bar"}}}
+
+
+def test_move_field_with_existing_siblings():
+    assert move_field({"foo": "bar", "level1": {"baz": "qux"}}, "foo", "level1.foo") == {"level1": {"foo": "bar", "baz": "qux"}}
 
 
 def test_move_field_without_nesting():
@@ -40,7 +52,7 @@ def test_get_date_from_task_for_batch_search():
 
 def test_get_date_from_task_for_tasks():
     a_date = datetime.datetime.fromisoformat("2024-08-20T07:27:42.192")
-    date = get_date_from_task({"@type":"Task","id":"771c45b4-1fac-421f-9791-64ac4e1eb4ab","name":"my_task", "createdAt": a_date.timestamp()})
+    date = get_date_from_task({"@type":"Task","id":"771c45b4-1fac-421f-9791-64ac4e1eb4ab","name":"my_task", "createdAt": a_date.timestamp() * 1000})
     assert date == a_date
 
 
@@ -48,3 +60,11 @@ def test_change_value():
     assert change_value({"foo": "bar"}, "foo", lambda v: v + "_blah") == {"foo": "bar_blah"}
     assert change_value({"foo": {"level1": 1}}, "foo.level1", lambda v: v+1) == {"foo": {"level1": 2}}
     assert change_value({"foo": {"level1": 1}, "bar": "baz"}, "foo.level1", lambda v: v+1) == {"foo": {"level1": 2}, "bar": "baz"}
+
+
+def test_change_value_with_existing_siblings():
+    assert change_value({"foo": {"level1": 1, "bar": "baz"}}, "foo.level1", lambda v: v+1) == {"foo": {"level1": 2, "bar": "baz"}}
+
+
+def test_change_value_with_list():
+    assert change_value({"foo": {"level1": [{"field": 1}, {"field": 2}]}}, "foo.level1[].field", lambda v: v+1) == {"foo": {"level1": [{"field": 2}, {"field": 3}]}}
