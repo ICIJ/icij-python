f11e92e2b539570344529fa9e12873036b5c53e7
Clément Doumouro <clement.doumouro@gmail.com>
2024-11-15 10:10:53 +0100

    chore: delete mistakenly versioned file

--- icij-worker/my_app.py
+++
@@ -1,9 +0,0 @@
-from icij_worker import AsyncApp
-
-my_app = AsyncApp(name="my_app")
-
-
-@my_app.task
-def long_running_task(greeted: str) -> str:
-    greeting = f"Hello {greeted} !"
-    return greeting
